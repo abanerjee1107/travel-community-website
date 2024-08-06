70ae974f92a0cb4a64bfc22f75a68e6c46dfaa89
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-08-06 19:58:25 +0530

    Create BlogList.js

---
+++ frontend/src/components/Blog/BlogList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BlogList = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/blogs/posts');
+                setBlogs(response.data);
+            } catch (error) {
+                console.error('Failed to fetch blogs:', error);
+            }
+        };
+        fetchBlogs();
+    }, []);
+
+    return (
+        <div>
+            <h1>Blog Posts</h1>
+            {blogs.map(blog => (
+                <div key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.content}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default BlogList;
