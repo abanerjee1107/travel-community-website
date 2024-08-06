099e4734736c837085fef71c4b7f9163dfaa44b9
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-08-06 20:12:31 +0530

    Create AboutUs.js

---
+++ frontend/src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>We are a community of travel enthusiasts sharing tips, planning trips, and connecting with each other.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
