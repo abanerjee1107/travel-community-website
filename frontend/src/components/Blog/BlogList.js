95434a234d6f54c278cfe9c609d8d6370701c3da
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-08-07 11:09:50 +0530

    Update BlogList.js

--- frontend/src/components/Blog/BlogList.js
+++ frontend/src/components/Blog/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = () => {
     useEffect(() => {
         const fetchBlogs = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/blogs/posts');
+                const response = await axios.get('http://ec2-54-91-3-52.compute-1.amazonaws.com:3000/blogs/posts');
                 setBlogs(response.data);
             } catch (error) {
                 console.error('Failed to fetch blogs:', error);
