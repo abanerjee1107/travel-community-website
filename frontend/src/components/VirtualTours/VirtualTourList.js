221bf5ca675f57d28e56ff4b1ccdecba4670cef9
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-08-07 11:13:14 +0530

    Update VirtualTourList.js

--- frontend/src/components/VirtualTours/VirtualTourList.js
+++ frontend/src/components/VirtualTours/VirtualTourList.js
@@ -7,7 +7,7 @@ const VirtualTourList = () => {
     useEffect(() => {
         const fetchTours = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/virtual-tours');
+                const response = await axios.get('http://ec2-54-91-3-52.compute-1.amazonaws.com:3000/virtual-tours');
                 setTours(response.data);
             } catch (error) {
                 console.error('Failed to fetch virtual tours:', error);
