aa1701d2597b528c99d6532482b6bfebf9c36420
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-08-07 11:14:18 +0530

    Update Reviews.js

--- frontend/src/components/Profile/Reviews.js
+++ frontend/src/components/Profile/Reviews.js
@@ -7,7 +7,7 @@ const Reviews = () => {
     useEffect(() => {
         const fetchReviews = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/reviews');
+                const response = await axios.get('http://ec2-54-91-3-52.compute-1.amazonaws.com:3000/reviews');
                 setReviews(response.data);
             } catch (error) {
                 console.error('Failed to fetch reviews:', error);
