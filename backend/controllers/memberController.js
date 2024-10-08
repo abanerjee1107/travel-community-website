3a8dbf6854aeffb03f355799a3b002bb897def38
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:06 +0530

    Update memberController.js

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,1 +1,18 @@
+const Member = require('../models/Member');
 
+exports.getMembers = async (req, res) => {
+    const members = await Member.findAll();
+    res.json(members);
+};
+
+exports.createMember = async (req, res) => {
+    const { destination, budget, currency, language } = req.body;
+    const member = await Member.create({
+        user_id: req.user.id,
+        destination,
+        budget,
+        currency,
+        language,
+    });
+    res.status(201).json(member);
+};
