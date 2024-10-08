5df355c320f15b0f689fbd882ede20ebc4f85851
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:17 +0530

    Update messageController.js

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,1 +1,16 @@
+const Message = require('../models/Message');
 
+exports.getMessages = async (req, res) => {
+    const messages = await Message.findAll({ where: { receiver_id: req.user.id } });
+    res.json(messages);
+};
+
+exports.sendMessage = async (req, res) => {
+    const { receiver_id, content } = req.body;
+    const message = await Message.create({
+        sender_id: req.user.id,
+        receiver_id,
+        content,
+    });
+    res.status(201).json(message);
+};
