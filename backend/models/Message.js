f779be0d5937baebf8fd2b245f7cdc3e6f83d7b7
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:59 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,1 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const Message = sequelize.define('Message', {
+    sender_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    receiver_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+});
+
+module.exports = Message;
