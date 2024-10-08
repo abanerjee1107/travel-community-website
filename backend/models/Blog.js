97b5c37e6a09b76af448a794b38dce89f342d3f3
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:38 +0530

    Update Blog.js

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,1 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const Blog = sequelize.define('Blog', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    author_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+module.exports = Blog;
