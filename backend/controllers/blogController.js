85e2bc8b72b53bc4e5478ec3ea6f77c3744c6f69
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:21:52 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,1 +1,12 @@
+const Blog = require('../models/Blog');
 
+exports.getBlogs = async (req, res) => {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+};
+
+exports.createBlog = async (req, res) => {
+    const { title, content } = req.body;
+    const blog = await Blog.create({ title, content, author_id: req.user.id });
+    res.status(201).json(blog);
+};
