01ceca4de647beb23c5ec932950e3a42c39e9e58
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:27:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const memberRoutes = require('./routes/memberRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const tripRoutes = require('./routes/tripRoutes');
+const sequelize = require('./config/database');
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+app.use('/blogs', blogRoutes);
+app.use('/members', memberRoutes);
+app.use('/messages', messageRoutes);
+app.use('/trips', tripRoutes);
+
+sequelize.sync().then(() => {
+    console.log('Database connected');
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
