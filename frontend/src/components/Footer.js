aa2602992b941b081c23326cbf509afcedecb7f7
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:19:04 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2024 Travel Community. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
 
