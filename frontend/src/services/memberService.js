796921a9d5c3460d85f926c6d75f88343fcca247
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:21:21 +0530

    Update memberService.js

--- frontend/src/services/memberService.js
+++ frontend/src/services/memberService.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+const API_URL = 'http://localhost:5000/members';
+
+const getMembers = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Get members error:', error);
+        return [];
+    }
+};
+
+export default {
+    getMembers,
+};
