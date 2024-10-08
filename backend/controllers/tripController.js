0dc3c2ac48f7741a34921e7176b75173f88c1f1e
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:25 +0530

    Update tripController.js

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -1,1 +1,16 @@
+const Trip = require('../models/Trip');
 
+exports.getTrips = async (req, res) => {
+    const trips = await Trip.findAll({ where: { user_id: req.user.id } });
+    res.json(trips);
+};
+
+exports.createTrip = async (req, res) => {
+    const { destination, budget } = req.body;
+    const trip = await Trip.create({
+        user_id: req.user.id,
+        destination,
+        budget,
+    });
+    res.status(201).json(trip);
+};
