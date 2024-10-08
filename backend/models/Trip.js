d88eb277266b4bb9144468a667c450640b36af28
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:23:08 +0530

    Update Trip.js

--- backend/models/Trip.js
+++ backend/models/Trip.js
@@ -1,1 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const Trip = sequelize.define('Trip', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    destination: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    budget: {
+        type: DataTypes.DECIMAL(10, 2),
+    },
+});
+
+module.exports = Trip;
