7e67b56cb91061263cac33a2bc5583ddc7af81b2
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:24:01 +0530

    Update tripRoutes.js

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const { getTrips, createTrip } = require('../controllers/tripController');
+const authMiddleware = require('../middleware/authMiddleware');
+const router = express.Router();
 
+router.get('/', authMiddleware, getTrips);
+router.post('/', authMiddleware, createTrip);
+
+module.exports = router;
