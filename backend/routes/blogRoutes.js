ccc9f6c6887d1b9a71033ee2731a8d1dfd6d08d6
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:23:33 +0530

    Update blogRoutes.js

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const { getBlogs, createBlog } = require('../controllers/blogController');
+const authMiddleware = require('../middleware/authMiddleware');
+const router = express.Router();
 
+router.get('/', getBlogs);
+router.post('/', authMiddleware, createBlog);
+
+module.exports = router;
