93bac9d634314bd4672c69ec31620d7d890424c3
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:21:08 +0530

    Update blogService.js

--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+const API_URL = 'http://localhost:5000/blogs';
+
+const getBlogs = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Get blogs error:', error);
+        return [];
+    }
+};
+
+export default {
+    getBlogs,
+};
