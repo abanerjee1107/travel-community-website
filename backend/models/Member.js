2cbbcff2e35cfc05267da3a998c182e0beeef80e
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:22:50 +0530

    Update Member.js

--- backend/models/Member.js
+++ backend/models/Member.js
@@ -1,1 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const Member = sequelize.define('Member', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    destination: {
+        type: DataTypes.STRING,
+    },
+    budget: {
+        type: DataTypes.DECIMAL(10, 2),
+    },
+    currency: {
+        type: DataTypes.STRING,
+    },
+    language: {
+        type: DataTypes.STRING,
+    },
+});
+
+module.exports = Member;
