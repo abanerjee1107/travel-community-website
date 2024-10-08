5c526bc2bad6041561af6e8c8325428115b4d198
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:20:16 +0530

    Update Blog.js

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,1 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import blogService from '../services/blogService';
 
+function Blog() {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            const fetchedBlogs = await blogService.getBlogs();
+            setBlogs(fetchedBlogs);
+        };
+        fetchBlogs();
+    }, []);
+
+    return (
+        <div>
+            <h1>Blog</h1>
+            <ul>
+                {blogs.map((blog) => (
+                    <li key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Blog;
