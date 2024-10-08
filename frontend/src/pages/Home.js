051341eb2137878d5147b251b2414ad943775fde
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:19:19 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Travel Community</h1>
+            <p>Connect with like-minded travelers and plan your next adventure!</p>
+        </div>
+    );
+}
+
+export default Home;
