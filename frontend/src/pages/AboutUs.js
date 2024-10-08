dff8e371cec21f8589a6b6b147991b9dcf5b7414
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:19:29 +0530

    Update AboutUs.js

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>We are a community of passionate travelers who love to explore the world and share experiences.</p>
+        </div>
+    );
+}
+
+export default AboutUs;
