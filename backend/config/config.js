588a08482c93b5f4364a9903ebe18b8cfcd34bb3
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:26:14 +0530

    Delete backend/config/config.js

--- backend/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    secret: 'your_jwt_secret_key',
-    db: {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'travel_community',
-    },
-};
