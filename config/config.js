73e0f467ad94d268bd582e243f2222af1a583867
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:26:35 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    secret: 'your_jwt_secret_key',
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'travel_community',
+    },
+};
