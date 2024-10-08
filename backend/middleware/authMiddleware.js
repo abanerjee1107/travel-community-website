adb9e38240b3e814325bdd991055d5a3c03d7227
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-10-08 18:15:42 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,13 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+    try {
+        const decoded = jwt.verify(token, 'secret');
+        req.user = decoded;
+        next();
+    } catch (ex) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
